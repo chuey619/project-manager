2960aa871c0b26662576040a2a18f44d98cebcf5
chueyi619 <chuey.israel@gmail.com>
2020-09-24 14:55:23 -0700

    changes

--- db/migrations/012209182020-create-projects-table.sql
+++ db/migrations/012209182020-create-projects-table.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS projects (
     id SERIAL PRIMARY KEY,
     team_id INTEGER REFERENCES teams(id),
     name VARCHAR(255),
+    description VARCHAR(255),
+    is_completed BOOLEAN DEFAULT FALSE,
     UNIQUE(team_id, name)
 );
--- db/migrations/093109242020-create-messages-table.sql
+++ db/migrations/093109242020-create-messages-table.sql
@@ -2,7 +2,8 @@ CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     sender_id INTEGER,
     project_id INTEGER,
-    
+    sender VARCHAR(255),
+    body VARCHAR(255),
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (project_id) REFERENCES projects(id)
 );
